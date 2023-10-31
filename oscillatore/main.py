c3a1e1a62f60cc32403168b4d6ddca4f0e4cc2de
eu <82298389+EugenioBarbieriViale@users.noreply.github.com>
2023-10-31 17:59:03 +0100

    Update main.py

--- oscillatore/main.py
+++ oscillatore/main.py
@@ -28,6 +28,10 @@ a = [F[0]/m]
 x_fin = []
 v_fin = []
 
+ki = [0.5*m*vi**2]
+u = [0.5*k*A**2]
+em = [ki[0]+u[0]]
+
 # Variables
 T = round(2 * np.pi * np.sqrt(m/k),4)
 v_ang = np.sqrt(k/m)
@@ -35,7 +39,7 @@ x_th = [A]
 
 
 for i in n:
-    x_fin.append(x_in[i] + v_in[i] * Dt + 0.5 * a[i] * Dt*Dt)
+    x_fin.append(x_in[i] + v_in[i] * Dt + 0.5 * a[i] * Dt**2)
     v_fin.append(v_in[i] + a[i] * Dt)
 
     F.append(-k * x_in[i])
@@ -46,8 +50,12 @@ for i in n:
 
     x_th.append(A * np.cos(v_ang*t[i]))
 
+    ki.append(0.5 * m * v_in[i]**2)
+    u.append(0.5 * k * x_in[i]**2)
+    em.append(ki[i] + u[i])
 
-# Graph 1
+
+# Position graph
 plt.figure()
 plt.plot(axis,x_in, label="x_in")
 plt.hlines(0,0,N,colors="black")
@@ -58,7 +66,7 @@ plt.ylabel("x(m)")
 plt.grid(visible=True)
 plt.legend()
 
-# Graph 2
+# Position graph (and theoric)
 plt.figure()
 
 plt.plot(axis,x_in, label="x_in")
@@ -69,6 +77,39 @@ plt.hlines(0,0,N,colors="black")
 plt.title("Periodo: {}s".format(str(T)))
 plt.xlabel("t(s)")
 plt.ylabel("x(m)")
+plt.grid(visible=True)
+plt.legend()
+
+# Kinetic energy
+plt.figure()
+plt.plot(axis,ki, label="k")
+plt.hlines(0,0,N,colors="black")
+
+plt.title("Energia cinetica")
+plt.xlabel("t(s)")
+plt.ylabel("k(J)")
+plt.grid(visible=True)
+plt.legend()
+
+# Potential energy
+plt.figure()
+plt.plot(axis,u, label="u")
+plt.hlines(0,0,N,colors="black")
+
+plt.title("Energia potenziale")
+plt.xlabel("t(s)")
+plt.ylabel("u(J)")
+plt.grid(visible=True)
+plt.legend()
+
+# Mechanical energy
+plt.figure()
+plt.plot(axis,em, label="em")
+plt.hlines(0,0,N,colors="black")
+
+plt.title("Energia meccanica")
+plt.xlabel("t(s)")
+plt.ylabel("e(J)")
 plt.grid(visible=True)
 plt.legend()
 
