1d7182e76c436811b55e49b8fe30da9fe67138d3
eu <82298389+EugenioBarbieriViale@users.noreply.github.com>
2023-11-13 21:30:17 +0100

    Update main.py

--- caduta/main.py
+++ caduta/main.py
@@ -27,6 +27,10 @@ a = [F[0]/m]
 x_fin = []
 v_fin = []
 
+ki = [0.5*m*vi**2]
+u = [m*g*x_in[0]]
+em = [ki[0]+u[0]]
+
 for i in n:
     x_fin.append(x_in[i] + v_in[i] * Dt - 0.5 * a[i] * Dt*Dt)
     v_fin.append(v_in[i] + a[i] * Dt)
@@ -36,6 +40,10 @@ for i in n:
 
     x_in.append(x_fin[i])
     v_in.append(v_fin[i])
+
+    ki.append(0.5 * m * v_in[i]**2)
+    u.append(m * g * x_in[i])
+    em.append(ki[i] + u[i])
 
 # Graph 1
 plt.figure()
@@ -54,6 +62,17 @@ plt.plot(axis,v_in, label="v_in", color="red")
 plt.title("Velocità")
 plt.xlabel("t(s)")
 plt.ylabel("v(m/s)")
+plt.grid(visible=True)
+plt.legend()
+
+# Mechanical energy
+plt.figure()
+plt.plot(axis,em, label="em")
+plt.hlines(0,0,N,colors="black")
+
+plt.title("Energia meccanica")
+plt.xlabel("t(s)")
+plt.ylabel("e(J)")
 plt.grid(visible=True)
 plt.legend()
 
